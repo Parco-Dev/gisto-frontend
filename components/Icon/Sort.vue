b08ef01e264b8d2360b5c585e3508b3be880e314
Gianluca Monaco <mckenzie.gian@gmail.com>
2023-11-06 01:21:18 +0100

    Create IconSort component

---
+++ components/Icon/Sort.vue
@@ -0,0 +1,5 @@
+<template>
+<svg id="Livello_1" class="sort-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 13.1 11.4" style="enable-background:new 0 0 13.1 11.4;" xml:space="preserve">
+  <polygon points="6.6,0 0,11.4 13.1,11.4 "/>
+</svg>
+</template>

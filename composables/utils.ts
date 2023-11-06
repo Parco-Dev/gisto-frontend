8c0085ab6209f184ae358f2eb5bdfaec22d1dafa
Davide Giorgetta <info@davidegiorgetta.com>
2023-11-06 19:00:20 +0100

    Update constructImageUrl function

--- composables/utils.ts
+++ composables/utils.ts
@@ -1,6 +1,6 @@
 export const constructImageUrl = (relativePath: string) => {
   // Replace "file://" with the base URL or path to your images
-  const baseUrl = 'http://gisto-backend.test/@/file/'; // Replace with your actual URL
+  const baseUrl = 'https://api.gianluca.space/gisto/@/file/'; // Replace with your actual URL
   return `${baseUrl}/${relativePath.replace('file://', '')}`;
 }
 

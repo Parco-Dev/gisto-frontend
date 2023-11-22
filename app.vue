d5cf5b9efb430016aae8efe25289b50f68b535ac
Gianluca Monaco <mckenzie.gian@gmail.com>
2023-11-22 10:18:22 +0100

    Page transition: wipe (initial)

--- app.vue
+++ app.vue
@@ -18,17 +18,54 @@
   </Html>
 </template>
 
-<style>
+<style lang='scss'>
   .page-enter-active,
   .page-leave-active {
-    transition: all 0.5s;
+    transition: all .5s;
+
+    p {
+      position: relative;
+      
+      &:after {
+        content: '';
+        display: inline-block;
+        position: absolute;
+        background-color: white;
+        width: 0px;
+        height: 100%;
+        top: 0;
+        left: 0;
+        transition: all .5s ease-in-out;
+        /* transition-delay: 1s; */
+      }
+    }
   }
-  .page-enter-from {
-    opacity: 0;
-    transform: translateX(-100vw);
+
+  @keyframes wipe {
+    0%   { width: 100% }
+    100% { width: 0 }
   }
+
+  .page-enter-to {
+    /* opacity: 0; */
+    /* transform: translateY(20px); */
+    /* transition-delay: .5s; */
+
+    p:after {
+      width: 100%;
+      animation: wipe .5s ease-in-out 1;
+    }
+  }
+
   .page-leave-to {
     opacity: 0;
-    transform: translateX(100vw);
+    transform: translateY(20px);
+    /* transition-delay: .5s; */
+
+    p:after {
+      width: 100%;
+      animation: wipe 1s ease-in-out 1;
+      animation-delay: .5s;
+    }
   }
 </style>
